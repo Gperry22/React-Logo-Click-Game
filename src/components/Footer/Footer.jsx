8b76088aed1b0057f8bc05e732b00f21e28bbf94
Gabe Perry <gperry22@gmaili.com>
2018-04-15 23:49:48 -0400

    Uploading

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css';
 
 const Footer = () => (
 <div className='container-fluid text-center footer'>
-<p>&copy; &nbsp; Gabe Perry &nbsp; <a href="http://" target='-blank'><i className='fab fa-github'></i></a></p>
+        <p>&copy; &nbsp; Gabe Perry &nbsp; <a href="https://github.com/Gperry22/React-Logo-Click-Game" target='-blank'><i className='fab fa-github'></i></a></p>
 </div>
 )
 
